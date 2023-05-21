084a29588399a7ed1d04bba10e07744c88e757ea
jyh4479 <jyh.dev@gmail.com>
2023-05-21 21:06:35 +0900

    [FEAT] Input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Input = (props) => {
+    return (
+        <InputStyle {...props}/>
+    )
+};
+
+export default Input;
+
+const InputStyle = styled.input`
+
+`;
---
+++ src/components/Input/index.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Input from '../Input';
+
+export default {
+    title: 'Components/Input',
+    component: Input,
+};
+
+export const Primary = {
+    args: {},
+};
