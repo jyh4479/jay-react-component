ac53c13d32d2ad8cf8fba8542722786873271179
jyh4479 <jyh.dev@gmail.com>
2023-12-23 14:42:07 +0900

    setting: mono repo component library
    - 모노레포 기반 컴포넌트 라이브러리 기본 패키지 설치
    - 배포할 컴포넌트의 package.json 확인 필요

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+    stories: ['packages/**/*.stories.tsx'],
+    addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+};
---
+++ packages/button/src/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = () => {
+    return (
+        <button>확인</button>
+    )
+}
+
+export default Button;
