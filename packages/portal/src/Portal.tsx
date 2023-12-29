c4236295bc68ec1d5c34854690a3fd1cc4140be2
jyh4479 <jyh.dev@gmail.com>
2023-12-29 16:53:45 +0900

    add: portal component

--- packages/portal/src/Portal.tsx
+++ packages/portal/src/Portal.tsx
@@ -1,9 +1,34 @@
-import {FunctionComponent} from "react";
+import React, {FunctionComponent, ReactNode, useEffect} from "react";
+import {createPortal} from "react-dom";
 
-const Portal: FunctionComponent = () => {
-    return (
-        <div>portal</div>
-    )
+interface propsType {
+    children?: ReactNode
 }
+
+const PORTAL_ID = "jay-portal-root";
+
+const Portal: FunctionComponent<propsType> = (props: propsType) => {
+
+    const {children} = props;
+
+    let portalRoot = document.getElementById(PORTAL_ID);
+
+    if (!portalRoot) {
+        portalRoot = document.createElement("div");
+        portalRoot.id = PORTAL_ID;
+        document.body.appendChild(portalRoot);
+    }
+
+    const el = document.createElement("div");
+
+    useEffect(() => {
+        portalRoot?.appendChild(el);
+        return () => {
+            portalRoot?.removeChild(el);
+        };
+    }, []);
+
+    return createPortal(children, el);
+};
 
 export default Portal;
