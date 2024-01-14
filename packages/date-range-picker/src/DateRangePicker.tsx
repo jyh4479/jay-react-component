698bf93ea20a669e313f186fb7d20d6327924c06
jyh4479 <jyh.dev@gmail.com>
2024-01-14 22:11:33 +0900

    [feat] focus state
    - focus 동작시 상태 관리
    - 로직다시 확인 필요

--- packages/date-range-picker/src/DateRangePicker.tsx
+++ packages/date-range-picker/src/DateRangePicker.tsx
@@ -1,5 +1,5 @@
-import React, {FunctionComponent, useState} from "react";
-import {SELECT_MODE} from "./data";
+import React, {FunctionComponent, useEffect, useState} from "react";
+import {DateMode, FocusState, SELECTED_FOCUS, SELECTED_MODE} from "./data";
 import {Calendar as CalendarSvg, SwapRight as SwapRightSvg} from "./svg";
 import Calendar from "./Calendar";
 import "./index.css";
@@ -10,22 +10,38 @@ const DateRangePicker: FunctionComponent = () => {
     const [currentDate, setCurrentDate] = useState<number>(new Date().getTime());
     const [selectedStartDate, setSelectedStartDate] = useState<number>(-1);
     const [selectedEndDate, setSelectedEndDate] = useState<number>(-1);
-    const [selectMode, setSelectMode] = useState<string>(SELECT_MODE.DAY);
+    const [selectMode, setSelectMode] = useState<DateMode>(SELECTED_MODE.DAY);
+    const [focusState, setFocusState] = useState<FocusState>(SELECTED_FOCUS.NONE);
+
+    useEffect(() => {
+        console.log(focusState);
+    }, [focusState])
 
     return (
         <>
             <Portal>
                 <Calendar/>
             </Portal>
-            <div className={`date-picker-layout`}>
+            <div
+                className={`date-picker-layout`}
+                onClick={() => setFocusState(SELECTED_FOCUS.LEFT)}
+                onBlur={() => setFocusState(SELECTED_FOCUS.NONE)}>
                 <div>
-                    <input type="text" placeholder={"Start date"}/>
+                    <input
+                        onClick={e => e.stopPropagation()}
+                        onFocus={(e) => setFocusState(SELECTED_FOCUS.LEFT)}
+                        type="text"
+                        placeholder={"Start date"}/>
                 </div>
                 <div>
                     <SwapRightSvg className={`date-picker-arrow-svg`}/>
                 </div>
                 <div>
-                    <input type="text" placeholder={"End date"}/>
+                    <input
+                        onClick={e => e.stopPropagation()}
+                        onFocus={(e) => setFocusState(SELECTED_FOCUS.RIGHT)}
+                        type="text"
+                        placeholder={"End date"}/>
                 </div>
                 <div>
                     <CalendarSvg className={`date-picker-calendar-svg`}/>
