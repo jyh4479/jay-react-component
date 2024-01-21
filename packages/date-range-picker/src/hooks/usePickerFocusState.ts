eb6b9fa28e3c9596628fbf63e436a59a05b8d0e7
jyh4479 <jyh.dev@gmail.com>
2024-01-21 16:01:08 +0900

    [feat] focus 기능 구현
    - ui, 이벤트 관련 로직 분리

---
+++ packages/date-range-picker/src/hooks/usePickerFocusState.ts
@@ -0,0 +1,56 @@
+import {FocusEventHandler, MouseEventHandler, RefObject, useEffect, useState} from "react";
+import {FocusState, SELECTED_FOCUS} from "../data";
+
+const usePickerFocusState = (parentRef: RefObject<HTMLDivElement>, leftInputRef: RefObject<HTMLInputElement>, rightInputRef: RefObject<HTMLInputElement>) => {
+
+    const [focusState, setFocusState] = useState<FocusState>(SELECTED_FOCUS.NONE);
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleOutsideClick);
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, []);
+
+    const handleOutsideClick = (e: any) => {
+        if (parentRef.current && !parentRef.current.contains(e.target)) {
+            setFocusState(SELECTED_FOCUS.NONE);
+        }
+    };
+
+    const parentOnClick: MouseEventHandler<HTMLDivElement> = (e) => {
+        if (focusState === SELECTED_FOCUS.LEFT) {
+            leftInputRef.current?.focus();
+            return;
+        }
+
+        if (focusState === SELECTED_FOCUS.RIGHT) {
+            rightInputRef.current?.focus();
+            return;
+        }
+
+        leftInputRef.current?.focus();
+    }
+
+    const leftInputOnFocus: FocusEventHandler<HTMLInputElement> = () => {
+        setFocusState(SELECTED_FOCUS.LEFT);
+    }
+
+    const rightInputOnFocus: FocusEventHandler<HTMLInputElement> = () => {
+        setFocusState(SELECTED_FOCUS.RIGHT);
+    }
+
+    const childInputOnClick: MouseEventHandler<HTMLInputElement> = (e) => {
+        e.stopPropagation();
+    }
+
+    return {
+        focusState,
+        parentOnClick,
+        leftInputOnFocus,
+        rightInputOnFocus,
+        childInputOnClick
+    };
+}
+
+export default usePickerFocusState;
