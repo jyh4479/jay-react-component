6654d3f3fa69cddad33d9d17d34c7cdbfa51fd95
jyh4479 <jyh.dev@gmail.com>
2024-01-27 13:58:17 +0900

    [feat] Component position hook

--- packages/date-range-picker/src/hooks/useComponentPosition.ts
+++ packages/date-range-picker/src/hooks/useComponentPosition.ts
@@ -2,14 +2,65 @@ import {RefObject, useEffect, useState} from "react";
 
 const useComponentPosition = (componentRef: RefObject<HTMLElement>) => {
 
-    const [position, setPosition] = useState({x: 0, y: 0});
+    const [componentPosition, setComponentPosition] = useState({x: 0, y: 0});
+    const [windowRelativePosition, setWindowRelativePosition] = useState("middle");
+
+    const updateComponentPosition = () => {
+        if (componentRef.current) {
+            const currentComponentPosition = componentRef.current?.getBoundingClientRect();
+            setComponentPosition({x: currentComponentPosition?.x ?? 0, y: currentComponentPosition?.y ?? 0});
+        }
+    }
+
+    const updateWindowRelativePosition = () => {
+        if (componentRef.current) {
+            const rect = componentRef.current.getBoundingClientRect();
+            const windowHeight = window.innerHeight;
+            const windowMidPoint = window.scrollY + windowHeight / 2;
+
+            if (rect.bottom < windowMidPoint) {
+                setWindowRelativePosition('above');
+            } else if (rect.top > windowMidPoint) {
+                setWindowRelativePosition('below');
+            } else {
+                setWindowRelativePosition('middle');
+            }
+        }
+    }
 
     useEffect(() => {
-        const componentPosition = componentRef.current?.getBoundingClientRect();
-        setPosition({x: componentPosition?.x ?? 0, y: componentPosition?.y ?? 0});
+        console.log(windowRelativePosition);
+    }, [windowRelativePosition])
+
+    useEffect(() => {
+        updateComponentPosition();
+        updateWindowRelativePosition();
+        const handleResize = () => {
+            updateComponentPosition();
+            updateWindowRelativePosition();
+        }
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        }
+
     }, [componentRef])
 
-    return position;
+    useEffect(() => {
+        updateWindowRelativePosition();
+        const handleScroll = () => {
+            updateWindowRelativePosition();
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+
+    }, [componentRef])
+
+    return componentPosition;
 }
 
 export default useComponentPosition;
