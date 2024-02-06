57ddc7e4706257ae2edbc6f3407ec053c1a4e1df
jyh4479 <jyh.dev@gmail.com>
2024-02-06 18:52:15 +0900

    [feat] scroll direction
    - 작은 화면에서의 dialog 동작을 위한 scroll 방향 정보 추가

--- packages/date-range-picker/src/hooks/useDialogDirection.ts
+++ packages/date-range-picker/src/hooks/useDialogDirection.ts
@@ -1,29 +1,48 @@
-import {RefObject, useLayoutEffect, useState} from "react";
+import {RefObject, useLayoutEffect, useRef, useState} from "react";
 import {Direction} from "../types/common";
+
+type ScrollDirection = "UP" | "DOWN";
 
 const useDialogDirection = (componentRef: RefObject<HTMLElement>, trigger: Array<any>) => {
 
     const [direction, setDirection] = useState<Direction>("BELOW");
 
+    const scrollDirection = useRef<ScrollDirection>("DOWN");
+    const prevScrollY = useRef<number>(0);
+
+    const updateScrollDirection = () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > prevScrollY.current) {
+            scrollDirection.current = "DOWN";
+        } else if (currentScrollY < prevScrollY.current) {
+            scrollDirection.current = "UP";
+        }
+
+        prevScrollY.current = currentScrollY;
+    }
+
     useLayoutEffect(() => {
         if (!componentRef?.current) return;
 
-        const updateDirection = () => {
+        const updateDialogDirection = () => {
+
+            updateScrollDirection();
 
             const currentComponentPosition = componentRef.current?.getBoundingClientRect();
 
             if (!currentComponentPosition) return;
 
-            if (currentComponentPosition.bottom > window.innerHeight) setDirection("ABOVE");
-            else if (currentComponentPosition.top < 0) setDirection("BELOW");
+            if (currentComponentPosition.bottom > window.innerHeight && scrollDirection.current === "UP") setDirection("ABOVE");
+            else if (currentComponentPosition.top < 0 && scrollDirection.current === "DOWN") setDirection("BELOW");
         }
 
-        updateDirection();
+        updateDialogDirection();
 
-        window.addEventListener("scroll", updateDirection);
+        window.addEventListener("scroll", updateDialogDirection);
 
         return () => {
-            window.removeEventListener("scroll", updateDirection);
+            window.removeEventListener("scroll", updateDialogDirection);
         }
 
     }, [componentRef?.current, ...trigger])
