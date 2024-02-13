47f41d0c90cca869b4a841ef808be7100e5c0fb5
jyh4479 <jyh.dev@gmail.com>
2024-02-13 14:46:06 +0900

    [style] arrow svg
    arrow svg 수정

--- packages/date-range-picker/src/svg/LeftArrow.tsx
+++ packages/date-range-picker/src/svg/LeftArrow.tsx
@@ -5,9 +5,7 @@ import "../index.css"
 const LeftArrow: FunctionComponent<svgPropsType> = (props: svgPropsType) => {
     return (
         <svg viewBox="0 0 48 48" {...props} fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="48" height="48" fill="white" fill-opacity="0.01"/>
-            <path d="M31 36L19 24L31 12" stroke="black" stroke-width="4" stroke-linecap="round"
-                  stroke-linejoin="round"/>
+            <path d="M31 36L19 24L31 12" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     )
 }
--- packages/date-range-picker/src/svg/RightArrow.tsx
+++ packages/date-range-picker/src/svg/RightArrow.tsx
@@ -5,9 +5,7 @@ import "../index.css"
 const RightArrow: FunctionComponent<svgPropsType> = (props: svgPropsType) => {
     return (
         <svg viewBox="0 0 48 48" {...props} fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="48" height="48" fill="white" fill-opacity="0.01"/>
-            <path d="M19 12L31 24L19 36" stroke="black" stroke-width="4" stroke-linecap="round"
-                  stroke-linejoin="round"/>
+            <path d="M19 12L31 24L19 36" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     )
 }
