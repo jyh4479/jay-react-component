aed47693c1f2ec88dcdc833c50098262d813f494
jyh4479 <jyh.dev@gmail.com>
2024-02-29 23:55:28 +0900

    [feat] focus state 연동
    - focus에 따라 setState 함수 분기

--- packages/date-range-picker/src/hooks/useSelectDateAndTime.ts
+++ packages/date-range-picker/src/hooks/useSelectDateAndTime.ts
@@ -11,14 +11,16 @@ const useSelectDateAndTime = (focusState: FocusState, setFocusState: Dispatch<Se
     const [selectedTempEndTimestamp, setSelectedTempEndTimestamp] = useState<number>(-1);
 
     //TODO: focus 상태를 확인하여 start, end 중 어떤 시간을 수정할지 결정하기 - 최대한 분기 태워서 처리하지 않기
-    const timestampSetter: Record<FocusState, Function> = {
-        "NONE": () => null,
-        "LEFT": setSelectedEndTimestamp,
+    const timestampSetter: Record<Exclude<FocusState, "NONE">, Dispatch<SetStateAction<number>>> = {
+        "LEFT": setSelectedStartTimestamp,
         "RIGHT": setSelectedEndTimestamp
     }
 
     const onClickDate = (timestamp: number) => {
-        setSelectedStartTimestamp(prev => {
+
+        if (focusState === "NONE") return;
+
+        timestampSetter[focusState](prev => {
             const currentSelectedStartDate = new Date(prev);
             const inputDate = new Date(timestamp);
 
@@ -31,7 +33,10 @@ const useSelectDateAndTime = (focusState: FocusState, setFocusState: Dispatch<Se
     }
 
     const onClickHour = (hour: number) => {
-        setSelectedStartTimestamp(prev => {
+
+        if (focusState === "NONE") return;
+
+        timestampSetter[focusState](prev => {
             const currentSelectedStartDate = new Date(prev);
             currentSelectedStartDate.setHours(hour);
             return currentSelectedStartDate.getTime();
@@ -39,7 +44,10 @@ const useSelectDateAndTime = (focusState: FocusState, setFocusState: Dispatch<Se
     }
 
     const onClickMinute = (minute: number) => {
-        setSelectedStartTimestamp(prev => {
+
+        if (focusState === "NONE") return;
+
+        timestampSetter[focusState](prev => {
             const currentSelectedStartDate = new Date(prev);
             currentSelectedStartDate.setMinutes(minute);
             return currentSelectedStartDate.getTime();
@@ -47,7 +55,10 @@ const useSelectDateAndTime = (focusState: FocusState, setFocusState: Dispatch<Se
     }
 
     const onClickSecond = (second: number) => {
-        setSelectedStartTimestamp(prev => {
+
+        if (focusState === "NONE") return;
+
+        timestampSetter[focusState](prev => {
             const currentSelectedStartDate = new Date(prev);
             currentSelectedStartDate.setSeconds(second);
             return currentSelectedStartDate.getTime();
